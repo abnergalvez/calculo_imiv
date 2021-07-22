25296c224b1df6b6552541fc1ce366caaa6159ad
abnergalvez <agc005@gmail.com>
2021-07-21 23:17:02 -0400

    se agrega formulario de email envio resultados

--- resources/views/components/resultados/btn_volver.blade.php
+++ resources/views/components/resultados/btn_volver.blade.php
@@ -1,16 +1,28 @@
+<br><br>
 <div class="col-md-12 row">
-<a href="{{ route('inicio') }}" class="p-3 col-md-4 btn btn-secondary btn-lg" >
-    Solo Volver
-</a>
+    <div class="col-md-4">
+        <a href="{{ route('inicio') }}" class="btn btn-secondary btn" >
+            Solo Volver
+        </a>
+    </div>
 <div class="col-md-4">
 <form action="{{ route('mixto_guardar') }}"  method="post">
     @csrf
     <input type="hidden" name="proyecto" value="{{ $attributes['proyecto'] }}">
     <input type="hidden" name="datos_calculo" value="{{ $attributes['datos_calculo']}}" >
     <input type="hidden" name="sumatoria" value="{{ json_encode($attributes['sumatoria']) }}" >
-    <button type="submit" class=" btn btn-primary btn-lg " >
-    Guardar para Calculo MIXTO/ Volver
+    <button type="submit" class=" btn btn-primary" >
+    Guardar calculo Mixto & Volver
     </button>
 </form>
 </div>
+<div class="col-md-4">
+    <div class="input-group mb-4">
+        <input type="text" class="form-control" placeholder="Ingrese email destino" aria-label="Email destino" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-success" type="button">Enviar Resultados</button>
+        </div>
+      </div>
+</div>
+
 </div>
--- resources/views/components/resultados/btn_volver_mixto.blade.php
+++ resources/views/components/resultados/btn_volver_mixto.blade.php
@@ -1,10 +1,21 @@
+<br><br>
 <div class="col-md-12 row">
-<a href="{{ route('inicio') }}" class="col-md-4 btn btn-secondary btn-lg" >
-    Solo Volver
-</a>
+    <div class="col-md-4">
+        <a href="{{ route('inicio') }}" class="btn btn-secondary btn" >
+            Solo Volver
+        </a>
+    </div>
 <div class="col-md-4">
-    <a href="{{ route('mixto_borrar') }}" class="btn btn-primary btn-lg " >
-        Volver y Limpiar Calculo Mixto
+    <a href="{{ route('mixto_borrar') }}" class="btn btn-primary btn " >
+        Volver y limpiar calculo Mixto
     </a>
+</div>
+<div class="col-md-4">
+    <div class="input-group mb-4">
+        <input type="text" class="form-control" placeholder="Ingrese email destino" aria-label="Email destino" aria-describedby="basic-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-success" type="button">Enviar Resultados</button>
+        </div>
+      </div>
 </div>
 </div>
